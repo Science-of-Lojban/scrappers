c8d38f115d8b6f285c3dad277362e62570c2dd3a
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2017-04-02 14:01:50 +0300

    Create ExtractExamples.js

---
+++ ExtractExamples.js
@@ -0,0 +1,58 @@
+"use strict";
+const cheerio = require('cheerio');
+const request = require('sync-request');
+const fs = require('fs');
+const path = require("path-extra");
+
+const file = path.join(__dirname,"output.tsv");
+
+const url = "http://vrici.lojban.org/~gleki/cll/build/xhtml_nochunks/index.html";
+const reqandwrite = function(arg){
+  const res = request('GET', arg).getBody();
+  const $ = cheerio.load(res);
+  let AR=[];
+  $('div[class=example]').each(function() {
+    let acc='';
+    $('p[class=title]',this).each(function(){
+      acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+"\t";
+    });
+    $(this).find('tr[class=jbo]').each(function(){
+      let arr=[];
+      $('td',this).each(function(){
+        arr.push($(this).text().trim());
+      });
+      acc+=arr.join(" ").trim()+";";
+    });
+    $(this).find('p[class=jbophrase]').each(function(){
+      let arr=[];
+      $(this).each(function(){
+        arr.push($(this).text().trim());
+      });
+      acc+=arr.join(" ").trim()+";";
+    });
+    acc+="\t";
+    $(this).find('tr[class=gloss]').each(function(){
+      let arr=[];
+      $('td',this).each(function(){
+        arr.push($(this).text().trim());
+      });
+      acc+=arr.join(" ").trim()+";";
+    });
+    $('div[class=example-contents] > p',this).each(function(){
+      if (!$(this).hasClass('jbophrase')){
+        acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+";";
+      }
+    });
+    acc+="\t";
+    $(this).find('p[class=natlang]',this).each(function(){
+      acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+";";
+    });
+    AR.push(acc.replace(/;\t/g,'\t').replace(/;$/g,''));
+  });
+  const str=AR.join('\n');
+  //console.log(str);
+  fs.writeFileSync(file, str);
+  //return acc;
+}
+
+reqandwrite(url);
