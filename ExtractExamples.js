d1e6dd69934e20e57345e38cdecd302a351e4634
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2017-04-04 10:53:05 +0300

    Update ExtractExamples.js

--- ExtractExamples.js
+++ ExtractExamples.js
@@ -4,50 +4,58 @@ const request = require('sync-request');
 const fs = require('fs');
 const path = require("path-extra");
 
+const regExp = /\'([^']+?)\'/;
+
 const file = path.join(__dirname,"output.tsv");
 
-const url = "http://vrici.lojban.org/~gleki/cll/build/xhtml_nochunks/index.html";
+function n(n){
+    return n > 9 ? "" + n: "0" + n;
+}
+
+const url = "http://vrici.lojban.org/~gleki/index.html";
 const reqandwrite = function(arg){
   const res = request('GET', arg).getBody();
   const $ = cheerio.load(res);
   let AR=[];
-  $('div[class=example]').each(function() {
-    let acc='';
-    $('p[class=title]',this).each(function(){
-      acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+"\t";
-    });
-    $(this).find('tr[class=jbo]').each(function(){
-      let arr=[];
-      $('td',this).each(function(){
-        arr.push($(this).text().trim());
+  $('div').each(function() {
+    if ($(this).hasClass('example')){
+      let acc='';
+      $('p[class=title]',this).each(function(){
+        acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+"\t";
       });
-      acc+=arr.join(" ").trim()+";";
-    });
-    $(this).find('p[class=jbophrase]').each(function(){
-      let arr=[];
-      $(this).each(function(){
-        arr.push($(this).text().trim());
+      $(this).find('tr[class=jbo]').each(function(){
+        let arr=[];
+        $('td',this).each(function(){
+          arr.push($(this).text().trim());
+        });
+        acc+=arr.join(" ").trim()+";";
       });
-      acc+=arr.join(" ").trim()+";";
-    });
-    acc+="\t";
-    $(this).find('tr[class=gloss]').each(function(){
-      let arr=[];
-      $('td',this).each(function(){
-        arr.push($(this).text().trim());
+      $(this).find('p[class=jbophrase]').each(function(){
+        let arr=[];
+        $(this).each(function(){
+          arr.push($(this).text().trim());
+        });
+        acc+=arr.join(" ").trim()+";";
       });
-      acc+=arr.join(" ").trim()+";";
-    });
-    $('div[class=example-contents] > p',this).each(function(){
-      if (!$(this).hasClass('jbophrase')){
+      acc+="\t";
+      $(this).find('tr[class=gloss]').each(function(){
+        let arr=[];
+        $('td',this).each(function(){
+          arr.push($(this).text().trim());
+        });
+        acc+=arr.join(" ").trim()+";";
+      });
+      $('div[class=example-contents] > p',this).each(function(){
+        if (!$(this).hasClass('jbophrase')){
+          acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+";";
+        }
+      });
+      acc+="\t";
+      $(this).find('p[class=natlang]',this).each(function(){
         acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+";";
-      }
-    });
-    acc+="\t";
-    $(this).find('p[class=natlang]',this).each(function(){
-      acc+=$(this).text().trim().replace(/[\r\n]/g,'').trim()+";";
-    });
-    AR.push(acc.replace(/;\t/g,'\t').replace(/;$/g,''));
+      });
+      AR.push(acc.replace(/;\t/g,'\t').replace(/;$/g,'').replace(/\{2,\}+/g,' '));
+    }
   });
   const str=AR.join('\n');
   //console.log(str);
